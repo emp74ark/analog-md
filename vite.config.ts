08644f09f8a029a16fd794767efb834d5b01b811
Andrei Yurkouski <emp74ark@gmail.com>
2024-12-05 18:42:52 +0100

    fix: ssr true

--- vite.config.ts
+++ vite.config.ts
@@ -6,20 +6,24 @@ import analog from '@analogjs/platform';
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   build: {
-    target: ['es2020'],
+    target: [ 'es2020' ],
   },
   resolve: {
-    mainFields: ['module'],
+    mainFields: [ 'module' ],
   },
-  plugins: [analog({
-    ssr: false
-  })],
+  plugins: [
+    analog({
+      ssr: true,
+      prerender: {
+        routes: [ '/' ],
+      },
+    }) ],
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['src/test-setup.ts'],
-    include: ['**/*.spec.ts'],
-    reporters: ['default'],
+    setupFiles: [ 'src/test-setup.ts' ],
+    include: [ '**/*.spec.ts' ],
+    reporters: [ 'default' ],
   },
   define: {
     'import.meta.vitest': mode !== 'production',
